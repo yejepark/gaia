822b41ae62aea97c7e5c94e6359fb6502cea5109
yejepark <yejepark@gmail.com>
2024-04-11 03:31:11 +0900

    [frontend] Add rent-range filter dialog

--- frontend/src/utilities/methods.js
+++ frontend/src/utilities/methods.js
@@ -7,3 +7,10 @@ export function debounce(callback, wait) {
         }, wait);
     };
 }
+
+export function arrayRange(start, stop, step) {
+    return Array.from(
+        { length: (stop - start) / step + 1 },
+        (value, index) => start + index * step
+    );
+}
